00b5f64c4192a80766106efd377cf9701b43f58e
Divyanshu Jain <divyanshujain1906@gmail.com>
2026-09-01 23:44:52 +0530

    Time: 23 ms (39.7%), Space: 34.8 MB (76.27%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,23 +5,22 @@ public:
         int n = mat[0].size();
         queue<pair<int,int>>qu;
         vector<vector<int>>ans(m,vector<int>(n,-1));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
                 if(mat[i][j]==0){
-                    ans[i][j]=0;
                     qu.push({i,j});
-                
+                    ans[i][j]=0;
                 }
             }
         }
-        int dr[4] = {1,-1,0,0};
-        int dc[4] = {0,0,1,-1};
-        while(not qu.empty()){
+        int dx[4] = {1,-1,0,0};
+        int dy[4] = {0,0,1,-1};
+        while(!qu.empty()){
             auto [r,c] = qu.front();
             qu.pop();
             for(int k=0;k<4;k++){
-                int nr = r + dr[k];
-                int nc = c + dc[k];
+                int nr = r+dx[k];
+                int nc = c+dy[k];
                 if(nr>=0 and nc>=0 and nr<m and nc<n and ans[nr][nc]==-1){
                     ans[nr][nc] = 1+ans[r][c];
                     qu.push({nr,nc});
