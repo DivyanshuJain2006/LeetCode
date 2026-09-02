b58be7ffd3ef5fdb5139e66c484f231488d33617
Divyanshu Jain <divyanshujain1906@gmail.com>
2026-09-02 11:38:37 +0530

    Time: 13 ms (31.76%), Space: 22.5 MB (85.73%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,96 +1,62 @@
 class Solution {
 public:
-    int m, n;
-
-    int dr[4] = {1, -1, 0, 0};
-    int dc[4] = {0, 0, 1, -1};
-
-    void markIsland(vector<vector<int>>& grid,
-                    queue<pair<int, int>>& bfsQ,
-                    int r, int c) {
-
-        queue<pair<int, int>> q;
-
+    queue<pair<int,int>>qu;
+    int m;
+    int n;
+    int dx[4] = {1,-1,0,0};
+    int dy[4] = {0,0,1,-1};
+    void marksIsland(vector<vector<int>>&grid,int r,int c){
+        queue<pair<int,int>>q;
+        qu.push({r,c});
+        q.push({r,c});
         grid[r][c] = 2;
-        q.push({r, c});
-        bfsQ.push({r, c});
-
-        while (!q.empty()) {
-            auto [x, y] = q.front();
+        while(!q.empty()){
+            auto [r,c] = q.front();
             q.pop();
-
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dr[k];
-                int ny = y + dc[k];
-
-                if (nx >= 0 && nx < m &&
-                    ny >= 0 && ny < n &&
-                    grid[nx][ny] == 1) {
-
-                    grid[nx][ny] = 2;
-
-                    q.push({nx, ny});
-                    bfsQ.push({nx, ny});
+            for(int k=0;k<4;k++){
+                int nr = r+dx[k];
+                int nc = c+dy[k];
+                if(nr>=0 and nc>=0 and nr<m and nc<n and grid[nr][nc]==1){
+                    qu.push({nr,nc});
+                    q.push({nr,nc});
+                    grid[nr][nc] = 2;
                 }
             }
         }
     }
-
     int shortestBridge(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
-
-        // This queue will be used for the SECOND BFS
-        queue<pair<int, int>> q;
-
-        // Find and mark first island
         bool found = false;
-
-        for (int i = 0; i < m && !found; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    markIsland(grid, q, i, j);
-                    found = true;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    found =true;
+                    marksIsland(grid,i,j);
                     break;
                 }
             }
+            if(found) break;
         }
-
-        // Multi-source BFS from first island
         int moves = 0;
-
-        while (!q.empty()) {
-            int sz = q.size();
-
-            while (sz--) {
-                auto [r, c] = q.front();
-                q.pop();
-
-                for (int k = 0; k < 4; k++) {
-                    int nr = r + dr[k];
-                    int nc = c + dc[k];
-
-                    if (nr < 0 || nr >= m ||
-                        nc < 0 || nc >= n) {
-                        continue;
-                    }
-
-                    // Reached second island
-                    if (grid[nr][nc] == 1) {
-                        return moves;
-                    }
-
-                    // Expand through water
-                    if (grid[nr][nc] == 0) {
+        while(!qu.empty()){
+            int size = qu.size();
+            
+            while(size--){
+                auto[r,c] = qu.front();
+                qu.pop();
+                for(int k=0;k<4;k++){
+                    int nr = r+dx[k];
+                    int nc= c+dy[k];
+                    if(nr>=0 and nc>=0 and nr<m and nc<n and grid[nr][nc]!=2){
+                        if(grid[nr][nc]==1) return moves;
                         grid[nr][nc] = 2;
-                        q.push({nr, nc});
+                        qu.push({nr,nc});
                     }
                 }
             }
-
             moves++;
         }
-
         return -1;
     }
 };
