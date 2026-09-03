fdfccec25ff9a4b52cbdab5d8de52eba063d26f8
Divyanshu Jain <divyanshujain1906@gmail.com>
2026-09-04 00:13:20 +0530

    Time: 6 ms (96.66%), Space: 18.9 MB (75.73%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -5,29 +5,27 @@ public:
         int m = grid[0].size();
         int dx[4] = {0,0,1,-1};
         int dy[4] = {1,-1,0,0};
-        vector<vector<int>>dist(n,vector<int>(m,1e9));
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
         deque<pair<int,int>>dq;
         dq.push_back({0,0});
         dist[0][0]=0;
         while(!dq.empty()){
-            auto curr = dq.front();
+            auto [r,c] = dq.front();
             dq.pop_front();
-            int x = curr.first;
-            int y = curr.second;
-            int dir = grid[x][y];
+            int dir = grid[r][c];
             for(int i=0;i<4;i++){
-                int nx = x+dx[i];
-                int ny = y+dy[i];
+                int nr = r+dx[i];
+                int nc = c+dy[i];
                 int edgewt = 1;
                 if(i+1==dir) edgewt=0;
-                if(nx<n and ny<m and nx>=0 and ny>=0){
-                    if(dist[nx][ny]>dist[x][y]+edgewt){
-                        dist[nx][ny]=dist[x][y]+edgewt;
+                if(nr<n and nc<m and nr>=0 and nc>=0){
+                    if(dist[nr][nc]>dist[r][c]+edgewt){
+                        dist[nr][nc]=dist[r][c]+edgewt;
                         if(edgewt==1){
-                            dq.push_back({nx,ny});
+                            dq.push_back({nr,nc});
                         }
                         else{
-                            dq.push_front({nx,ny});
+                            dq.push_front({nr,nc});
                         }
                     }
                 
