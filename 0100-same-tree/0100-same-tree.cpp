8407802f4caf901c4d399f76d40fc4495c6a1dd7
Divyanshu Jain <divyanshujain1906@gmail.com>
2026-09-06 10:06:48 +0530

    Time: 0 ms (100%), Space: 12.6 MB (96.84%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,11 +14,11 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL and q==NULL) return true;
         if(p==NULL || q==NULL) return false;
-        if(p->val != q->val) return false;
+        if(p->val!=q->val) return false;
         bool leftAns = isSameTree(p->left,q->left);
-        if(leftAns==false) return false;
+        if(leftAns == false) return false;
         bool rightAns = isSameTree(p->right,q->right);
         if(rightAns==false) return false;
         return true;
-    }   
+    }
 };
