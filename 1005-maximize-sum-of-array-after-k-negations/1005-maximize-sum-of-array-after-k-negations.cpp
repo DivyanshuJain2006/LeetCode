c0fe4845d21afe79203720136b626c1ccedd1d2f
Divyanshu Jain <divyanshujain1906@gmail.com>
2026-09-12 19:41:08 +0530

    Time: 3 ms (22.93%), Space: 12.9 MB (26.42%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
@@ -4,15 +4,15 @@ public:
         int n = nums.size();
         int sum = 0;
         priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            sum += nums[i];
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
         }
         while(k--){
-            int ele = pq.top();
+            auto ele = pq.top();
             pq.pop();
             sum-=ele;
             pq.push(-1*ele);
-            sum += (-1*ele);
+            sum+=(-1*ele);
         }
         return sum;
     }
