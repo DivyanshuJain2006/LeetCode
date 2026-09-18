2ebb6dfee244dcb4a771376962fd5efef6c2dd0d
Divyanshu Jain <divyanshujain1906@gmail.com>
2026-09-18 21:42:30 +0530

    Time: 12 ms (81.03%), Space: 23.5 MB (91.11%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,11 +3,12 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        set<pair<int,int>>visited;
         queue<pair<int,int>>qu;
         int moves = 0;
-        if(grid[0][0]==0) qu.push({0,0});
-        visited.insert({0,0});
+        if(grid[0][0]==0){
+            qu.push({0,0});
+            grid[0][0]=1;
+        }
         int dx[8] = {1,-1,0,0,-1,-1,1,1};
         int dy[8] = {0,0,1,-1,-1,1,-1,1};
         while(!qu.empty()){
@@ -19,9 +20,10 @@ public:
             for(int k=0;k<8;k++){
                 int nr = r+dx[k];
                 int nc = c+dy[k];
-                if(nr>=0 and nc>=0 and nr<m and nc<n and grid[nr][nc]==0 and !visited.count({nr,nc})) {
+                if(nr>=0 and nc>=0 and nr<m and nc<n and grid[nr][nc]==0 ) {
+                    grid[nr][nc]=1;
                     qu.push({nr,nc});
-                    visited.insert({nr,nc});
+                   
                 }
               }
            }
