e7915cc35e2591e7e48d4dd6cf75a6801edbe876
Divyanshu Jain <divyanshujain1906@gmail.com>
2026-09-20 16:42:00 +0530

    Time: 0 ms (100%), Space: 14.4 MB (18.25%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        int maxi = INT_MIN;
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(auto ele : nums){
-            mp[ele]++;
-            if(mp[ele]==1){
-                maxi = max(ele,maxi);
-                pq.push(ele);
-                if(pq.size()>3) pq.pop();
-            }
-        }
-        if(pq.size()==3) return pq.top();
-        return maxi; 
+       set<int> s;
+
+for(auto x : nums) {
+    s.insert(x);
+    if(s.size() > 3)
+        s.erase(s.begin());
+}
+
+if(s.size() == 3)
+    return *s.begin();
+
+return *s.rbegin();
     }
 };
