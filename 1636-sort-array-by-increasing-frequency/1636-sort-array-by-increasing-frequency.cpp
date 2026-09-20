273fca98adb894ae9c5b8b30fe607d87315fcff5
Divyanshu Jain <divyanshujain1906@gmail.com>
2026-09-20 15:30:02 +0530

    Time: 4 ms (34.11%), Space: 15.2 MB (32.03%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
- typedef pair<int,int> pii;
-struct cmp {
-        bool operator()(pii a, pii b) {
-            if(a.first == b.first)
-                return a.second < b.second;
-            return a.first > b.first;
+    typedef pair<int,int>pii;
+    struct cmp{
+        bool operator()(pii a,pii b){
+            if(a.first==b.first) return b.second>a.second;
+            return a.first>b.first;
         }
     };
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int,int>mp;
         for(auto ele:nums) mp[ele]++;
         priority_queue<pii,vector<pii>,cmp>pq;
-        for(auto [ele,freq] : mp){
-            pq.push({freq,ele});
-        }
+        for(auto [ele,freq] : mp) pq.push({freq,ele});
         vector<int>ans;
         while(!pq.empty()){
-           auto [freq,ele] = pq.top();
-           pq.pop();
+            auto [freq,ele] = pq.top();
+            pq.pop();
             while(freq--) ans.push_back(ele);
         }
         return ans;
