c0f46169d33ecd58f6960c5729233eb34d543c1c
Divyanshu Jain <divyanshujain1906@gmail.com>
2026-09-23 19:26:08 +0530

    Time: 0 ms (100%), Space: 17.4 MB (52.94%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -1,43 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void fun(TreeNode* root,vector<int> v){
-    if(root == NULL) return;
-    v.push_back(root->val);
-    if(root->left == NULL && root->right == NULL){
-        ans.push_back(v);
-        return;
-    }
-    fun(root->left, v);
-    fun(root->right, v);
-    }
-    int binaryToDecimal(vector<int>& binary) {
-    int decimal = 0;
+    int dfs(TreeNode* root, int num) {
+        if(root == NULL)
+            return 0;
 
-    for (int bit : binary) {
-        decimal = decimal * 2 + bit;
+        num = num * 2 + root->val;
+
+        
+        if(root->left == NULL && root->right == NULL)
+            return num;
+
+        return dfs(root->left, num) + dfs(root->right, num);
     }
 
-    return decimal;
-}
     int sumRootToLeaf(TreeNode* root) {
-        vector<int>v;
-        fun(root,v);
-        int answer = 0;
-        for(int i=0;i<ans.size();i++){
-            answer += binaryToDecimal(ans[i]);
-        }
-        return answer;
+        return dfs(root, 0);
     }
 };
